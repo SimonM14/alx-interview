a0608387f43eb46565d647267087b6dd5eda6618
SimonM14 <kaninisimon79@gmail.com>
2023-08-22 11:00:54 +0000

    new file

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -3,8 +3,10 @@
 Main file for testing
 """
 
-makeChange = __import__('0-making_change').makeChange
+makeChange = __import__('make2').makeChange
 
 print(makeChange([1, 2, 25], 37))
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
+
+print(makeChange([1, 5, 10, 25], 63))
