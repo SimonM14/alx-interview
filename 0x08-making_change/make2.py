771dd7b6ad9a0a37d3fd4799e7342859fe10ca23
SimonM14 <kaninisimon79@gmail.com>
2023-08-22 11:12:05 +0000

    new file

---
+++ 0x08-making_change/make2.py
@@ -0,0 +1,15 @@
+def makeChange(coins, total):
+    if total <=0:
+        return 0
+    dp =  [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    return dp[total]
+                            
